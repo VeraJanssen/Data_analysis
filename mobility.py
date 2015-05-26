a45ad34f3c7d1ae96f159cada208ae655cd1bf49
unknown <verajanssen@TUD206905.tudelft.net>
2015-05-26 14:51:54 +0200

    Created the sketch outline for the script

--- mobility.py
+++ mobility.py
@@ -5,3 +5,25 @@ Created on Tue May 26 14:35:41 2015
 @author: verajanssen
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+ # ask for inputs:
+    #working folder
+    #device size
+    #....
+
+
+
+ # open IVsd files from IVsd folder
+
+ # open IVg files from IVg folder
+
+ # check for loop or seperate files
+
+ # plot both datasets in two figures in same window
+
+ # save the figure with foldername, filenames, githash
+
+ # calculate mobility, Rsq
