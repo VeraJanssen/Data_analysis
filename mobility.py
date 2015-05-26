88bf70b51d5b7181ffdb3c54763d47aedc3729d6
unknown <verajanssen@TUD206905.tudelft.net>
2015-05-26 15:28:50 +0200

    Add the normalized plots

--- mobility.py
+++ mobility.py
@@ -22,9 +22,9 @@ IVg = np.loadtxt('D:/verajanssen/megasweep2.dat')
 gate = np.linspace(0.5, -0.5, np.size(IVg,1))
 
 for i in range(np.size(IVg, 0)):
-    plt.plot(gate, IVg[i, :])
-plt.show()
-
+    rawIVg = plt.plot(gate, IVg[i, :])
+    #subtract the Vg = 0V (background) curve. Use the middle column. Will give error when the number of measurements is odd (than there is no Vg = 0V) 
+    normIVg = plt.plot(gate, IVg[i, :]-IVg[(np.size(IVg, 0)+1)/2])
 
 
  # open IVg files from IVg folder
